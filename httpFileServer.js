f7f42cd1d287adf30739878b5c543e63d55c97ef
mdouglasbrett <mdouglasbrett@icloud.com>
2015-04-20 14:12:11 +0100

    Added official solution to httpFileServer

--- httpFileServer.js
+++ httpFileServer.js
@@ -8,3 +8,22 @@ var server = http.createServer(function (req, res) {
 });
 
 server.listen(process.argv[2]);
+
+/* Official Solution
+
+var http = require('http')
+    var fs = require('fs')
+
+    var server = http.createServer(function (req, res) {
+      res.writeHead(200, { 'content-type': 'text/plain' })
+
+      fs.createReadStream(process.argv[3]).pipe(res)
+    })
+
+    server.listen(Number(process.argv[2]))
+
+I assumed the port supplied in process.argv[2] would be a number, but I suppose that's a bit naive.
+
+No need to explicitly assign a value to req(?)
+
+*/
